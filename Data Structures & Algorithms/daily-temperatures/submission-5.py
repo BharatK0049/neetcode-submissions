862aae6433a2bea5670554ea1de53ace621f5f44
Bharat Kameswaran <64674045+BharatK0049@users.noreply.github.com>
2026-07-16 20:39:16 +0530

    Add: daily-temperatures - submission-5

---
+++ Data Structures & Algorithms/daily-temperatures/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # O(N)
+        stack = [] # Stores indices of the next largest element
+        result = [0] * len(temperatures)
+        
+        for i in range(len(temperatures)-1, -1, -1):
+            while (stack != [] and stack[-1][0] <= temperatures[i]):
+                stack.pop()
+            
+            if stack == []:
+                result[i] = 0
+            else:
+                result[i] = stack[-1][1] - i
+            
+            stack.append((temperatures[i], i))
+        
+        return result
