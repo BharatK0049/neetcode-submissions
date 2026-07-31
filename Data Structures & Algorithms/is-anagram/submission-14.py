fd4d11d1fc3f24342ae068759ddfdadf2108e300
Bharat Kameswaran <64674045+BharatK0049@users.noreply.github.com>
2026-07-31 21:46:18 +0530

    Add: is-anagram - submission-14

---
+++ Data Structures & Algorithms/is-anagram/submission-14.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        hash_map = {}
+
+        for i in s:
+            if i in hash_map:
+                hash_map[i] += 1
+            else:
+                hash_map[i] = 1
+        
+        for i in t:
+            if i in hash_map:
+                hash_map[i] -= 1
+            else:
+                return False
+        
+        for i in hash_map:
+            if hash_map[i] != 0:
+                return False
+        
+        return True
